875f190c3e3392f68c65f7020fdf3b5cb9407cfe
hkhan <brad.linkan@gmail.com>
2013-07-05 12:12:08 +0800

    test for merge

--- pinloveweb/settings.py
+++ pinloveweb/settings.py
@@ -11,13 +11,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django',                      # Or path to database file if using sqlite3.
-        # The following sesdfsdttings are not used with sqlite3:
+
+        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'pinlove',                      # Or path to database file if using sqlite3.
+                                                # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'jin521436',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PASSWORD': 'sa',
+        'HOST': '',                             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                             # Set to empty string for default.
     }
 }
 
