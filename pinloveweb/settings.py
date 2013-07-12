20e75b9b96bf2ef3af397cd381e64b8cdc883ffe
SnailJin <jin521436@163.com>
2013-07-13 01:11:40 +0800

    test path

--- pinloveweb/settings.py
+++ pinloveweb/settings.py
@@ -51,12 +51,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT ='http://www.pinpinlove.com/static/update'
+MEDIA_ROOT ='/update'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'http://www.pinpinlove.com/static/update/'
+MEDIA_URL = '/update/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -66,12 +66,12 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = 'http://www.pinpinlove.com/static/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    ("assets", "http://www.pinpinlove.com/static"), 
-    ("update", "http://www.pinpinlove.com/update"),  
+    ("assets", "/static"), 
+    ("update", "/update"),  
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -114,8 +114,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-   "http://www.pinpinlove.com/templates", 
-   "http://www.pinpinlove.com/apps/user_app/templates", 
+   "/templates", 
+   "/apps/user_app/templates", 
 )
 
 INSTALLED_APPS = (
