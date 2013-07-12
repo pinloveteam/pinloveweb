ba19b99677e7ebda7847a3b12caa8e97f0d59ada
SnailJin <jin521436@163.com>
2013-07-13 00:20:57 +0800

    test server

--- pinloveweb/settings.py
+++ pinloveweb/settings.py
@@ -14,10 +14,10 @@ DATABASES = {
     'default': {
 
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django',                      # Or path to database file if using sqlite3.
+        'NAME': 'pinlove_db_1',                      # Or path to database file if using sqlite3.
                                                 # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'jin521436',
+        'USER': 'pinloveteam',
+        'PASSWORD': 'redyellowblue#123',
         'HOST': '',                             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                             # Set to empty string for default.
     }
@@ -52,12 +52,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = 'D:\eclipse\code\pinloveweb\update'
+MEDIA_ROOT ='http://www.pinpinlove.com/static/update'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://www.pinpinlove.com/static/update/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -67,12 +67,12 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://www.pinpinlove.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    ("assets", os.path.realpath(os.path.dirname(__file__))[:-11]+"\\static"), 
-    ("update", os.path.realpath(os.path.dirname(__file__))[:-11]+"\\update"),  
+    ("assets", "http://www.pinpinlove.com/static"), 
+    ("update", "http://www.pinpinlove.com/update"),  
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -115,8 +115,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.realpath(os.path.dirname(__file__))[:-11]+"\\templates", 
-    os.path.realpath(os.path.dirname(__file__))[:-11]+"\\apps\\user_app\\templates", 
+   "http://www.pinpinlove.com/templates", 
+   "http://www.pinpinlove.com/apps/user_app/templates", 
 )
 
 INSTALLED_APPS = (
