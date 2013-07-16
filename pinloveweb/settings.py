eb81de1cda5d74a31943a4a1ebf98d5496829fec
SnailJin <jin521436@163.com>
2013-07-17 01:12:51 +0800

    test paht

--- pinloveweb/settings.py
+++ pinloveweb/settings.py
@@ -57,7 +57,7 @@ MEDIA_ROOT = os.path.join(PATH,'update').replace('\\','/')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://www.pinpinlove.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -67,7 +67,7 @@ STATIC_ROOT = os.path.join(PATH,'static').replace('\\','/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://www.pinpinlove.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
