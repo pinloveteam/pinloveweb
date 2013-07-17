5b7d637f9ded8d636d14cfdaac2dc48f411150b3
SnailJin <jin521436@163.com>
2013-07-17 14:13:18 +0800

    test path

--- pinloveweb/settings.py
+++ pinloveweb/settings.py
@@ -58,7 +58,7 @@ MEDIA_ROOT ='/home/pinloveteam/webapps/pinlove/pinloveweb/update'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://www.pinpinlove.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -68,7 +68,7 @@ STATIC_ROOT = '/home/pinloveteam/webapps/pinlove/pinloveweb/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://www.pinpinlove.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
