f6cd652db879566b2e1976da2f06242852a4e0d8
SnailJin <jin521436@163.com>
2013-07-17 14:49:08 +0800

    test path

--- pinloveweb/settings.py
+++ pinloveweb/settings.py
@@ -1,7 +1,7 @@
 # Django settings for pinlove project.
 import os
 PATH=os.path.dirname(os.path.dirname(__file__))
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -23,10 +23,9 @@ DATABASES = {
         'PORT': '',                             # Set to empty string for default.
     }
 }
-
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -53,7 +52,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT ='/home/pinloveteam/webapps/pinlove/pinloveweb/update'
+MEDIA_ROOT = os.path.join(PATH,'update').replace('\\','/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +63,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/pinloveteam/webapps/pinlove/pinloveweb/static'
+STATIC_ROOT = os.path.join(PATH,'static').replace('\\','/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,9 +71,9 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    ('css','/home/pinloveteam/webapps/pinlove/pinloveweb/css' ),  
-    ('js','/home/pinloveteam/webapps/pinlove/pinloveweb/js' ), 
-    ('img','/home/pinloveteam/webapps/pinlove/pinloveweb/img' ), 
+    ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),  
+    ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ), 
+    ('img',os.path.join(STATIC_ROOT,'img').replace('\\','/') ), 
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -117,8 +116,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/pinloveteam/webapps/pinlove/pinloveweb/templates', 
-      '/home/pinloveteam/webapps/pinlove/pinloveweb/apps/user_app/templates', 
+    os.path.join(PATH,'templates').replace('\\','/'), 
+      os.path.join(PATH,'apps/user_app/templates').replace('\\','/'), 
 )
 
 INSTALLED_APPS = (
@@ -177,4 +176,3 @@ LOGGING = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = 'email_message/' # change this to a proper location
-
