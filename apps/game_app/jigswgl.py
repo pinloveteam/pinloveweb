3f348c99cc9b19c7587929c2ced1a701b57c770d
hkhan <brad.linkan@gmail.com>
2013-10-15 10:07:22 +0800

    pintu

--- apps/game_app/jigswgl.py
+++ apps/game_app/jigswgl.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+import datetime
+COUNT = 10 #游戏计数
 JIGSW_GIRLS = {} #存放girl数据
 JIGSW_BOYS = {}  #存放boy数据
-JIGSWABLE = set()#
+JIGSW_COUNT = {} #游戏次数
 UNIVERSAL_SET = set([0,1,2,3,4,5,6,7,8,9,10,11,13,14])
+TODAY = datetime.date.today()
+
+
 
