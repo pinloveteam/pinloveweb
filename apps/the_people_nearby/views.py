e1294f08a3eb0704698f712c4b7c1db020491d4f
Administrator <brad@ubuntu.ubuntu-domain>
2013-11-18 15:03:40 +0800

    the people nearby

--- apps/the_people_nearby/views.py
+++ apps/the_people_nearby/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-import urllib2
 from django.shortcuts import render
 from apps.user_app.models import UserProfile
+import httplib
+from xml.dom import minidom
 def GetIp(request):
     if 'HTTP_X_FORWARDED_FOR' in request.META:
         return request.META['HTTP_X_FORWARDED_FOR']
@@ -9,18 +10,27 @@ def GetIp(request):
         return request.META['REMOTE_ADDR']
     
 def GetLocation(request):  
-    try: 
-        url = 'http://api.map.baidu.com/location/ip?ak=E1356a909570fb0e255f1f278dabd11b&ip='+GetIp(request)
-        request = urllib2.Request(url)
-        response = urllib2.urlopen(request)
-        return response.read().split('|')[2] #decode('raw_unicode_escape')
+    httpClient = None
+    
+    try:
+        httpClient = httplib.HTTPConnection('192.151.154.154', 80, timeout=30)
+        httpClient.request('GET', '/xml/'+GetIp(request))
+    
+        response = httpClient.getresponse()
+        dom = minidom.parseString(response.read())      
+        root = dom.firstChild      
+        childs = root.childNodes    
+        for child in childs:  
+            if child.nodeName == 'City':
+                return child.childNodes[0].data
+    
     except Exception, e:
         print e
+    finally:
+        if httpClient:
+            httpClient.close()
         
 def the_people_nearby(request):
     arg = {}
     arg['the_people_nearby'] =  UserProfile.objects.filter(lastLoginAddress=GetLocation(request))
-#     arg['the_people_nearby']
-#     for userprofile in userprofiles:
-#         print userprofile.user.username
     return render(request, 'the_people_nearby.html',arg)    
