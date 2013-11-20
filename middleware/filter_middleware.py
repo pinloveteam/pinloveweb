f28e6f16742a344c6fdcae2acc013d389d8b7557
SnailJin <jin521436@163.com>
2013-11-20 16:01:48 +0800

    middleware

--- middleware/filter_middleware.py
+++ middleware/filter_middleware.py
@@ -3,14 +3,13 @@ Created on Nov 19, 2013
 
 @author: jin
 '''
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
 class AuthenticationMiddleware(object):   
     def process_request(self, request):  
-        if not (request.path == '/account/auth/' or request.path == '/'): 
-            arg={}
+        if not (request.path == '/account/auth/' or request.path == '/' or request.path.find('.')!=-1): 
             if request.user.is_authenticated():
                 return None  
             else:
-                return render(request,'login.html',arg)
+                return HttpResponseRedirect('/account/login/')
         else:
-            return None 
+            return None
