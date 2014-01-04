3f8c08b0af73374f9bfc6121d467ab1de3522723
Administrator <brad@ubuntu.ubuntu-domain>
2014-01-04 14:39:02 +0800

    pintu

--- apps/game_app/views.py
+++ apps/game_app/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import simplejson
 from apps.game_app.models import Yuanfenjigsaw, get_count
-
+import logging
+log=logging.getLogger('customapp.engine')
 @csrf_exempt
 def jigsaw(request):
     match_result = Yuanfenjigsaw(request).get_match_result()
-    return HttpResponse(simplejson.dumps(match_result), mimetype='application/javascript')
+    json=simplejson.dumps(match_result)
+    log.debug(json)
+    return HttpResponse(json, mimetype='application/javascript')
 
 def pintu(request):
     count = get_count(request)
