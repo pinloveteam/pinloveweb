bb6c55de8d983168905b8c45c1660529a13bdabf
Administrator <brad@ubuntu.ubuntu-domain>
2014-01-07 11:32:19 +0800

    pintu

--- apps/game_app/models.py
+++ apps/game_app/models.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from apps.user_app.models import UserProfile
-from apps.game_app import jiglobal
 import datetime
+
 from django.contrib.auth.models import User
+from django.core.cache import cache
 
 class Yuanfenjigsaw:
     selected_pieces_str = ''
@@ -10,38 +11,37 @@ class Yuanfenjigsaw:
     matching_pieces = set()
     current_username = ''
     gender = ''
-        
+       
     def __init__(self,request):
-        if jiglobal.TODAY != datetime.date.today():
+        if cache.get('TODAY') != datetime.date.today():
             reset_game()
             
         self.current_username = request.user.username
         self.selected_pieces_str=request.GET.get("selectedPieces",'')
         self.selected_pieces = set([int(i) for i in self.selected_pieces_str.split("-") if i])#用户提交的集合
-        self.matching_pieces =  jiglobal.ALL_PIECE - self.selected_pieces#与之互补的集合
+        self.matching_pieces =  cache.get('ALL_PIECE') - self.selected_pieces#与之互补的集合
         self.gender = UserProfile.objects.get(user=request.user).gender
         
     def data_unavailable(self):
-        return  self.selected_pieces - jiglobal.ALL_PIECE != set([]) or self.matching_pieces ==  jiglobal.ALL_PIECE or self.matching_pieces == set([])
+        return  self.selected_pieces - cache.get('ALL_PIECE') != set([]) or self.matching_pieces ==  cache.get('ALL_PIECE') or self.matching_pieces == set([])
     
     def achieve_game_times(self):
-        return  jiglobal.USER_GAME_COUNT.get(self.current_username) == 0
-
+        return  cache.get('USER_GAME_COUNT').get(self.current_username) == 0
+    
     def get_matching_user(self):
-        import logging
-        log=logging.getLogger('customapp.engine')
-        log.error(str(self.current_username))
-        from django.utils import simplejson
-        log.error(simplejson.dumps(jiglobal.USER_GAME_COUNT))
-        count=jiglobal.USER_GAME_COUNT.get(self.current_username) - 1
-        log.error(str(count))
-        jiglobal.USER_GAME_COUNT[self.current_username] = jiglobal.USER_GAME_COUNT.get(self.current_username) - 1
+        user_game_count = cache.get('USER_GAME_COUNT')
+        user_game_count[self.current_username] = user_game_count.get(self.current_username) - 1
+        cache.set('USER_GAME_COUNT',user_game_count)
         if  self.gender == 'M':
-            jiglobal.BOYS[str(self.selected_pieces)] = self.current_username#如果位男性，则将其存入JIGSW_BOYS
-            matching_username = jiglobal.GIRLS.get(str(self.matching_pieces))#从JIGSW_GIRLS中寻找与之互补的异性
+            boys = cache.get('BOYS')
+            boys[str(self.selected_pieces)] = self.current_username#如果位男性，则将其存入JIGSW_BOYS
+            matching_username = cache.get('GIRLS').get(str(self.matching_pieces))#从JIGSW_GIRLS中寻找与之互补的异性
+            cache.set('BOYS',boys)
         else :
-            jiglobal.GIRLS[str(self.selected_pieces)] = self.current_username#同上
-            matching_username =  jiglobal.BOYS.get(str(self.matching_pieces))
+            girls = cache.get('GIRLS')
+            girls[str(self.selected_pieces)] = self.current_username
+            matching_username =  cache.get('BOYS').get(str(self.matching_pieces))
+            cache.set('GIRLS',girls)
             
         if matching_username != None:
             matching_user = UserProfile.objects.get(user=User.objects.get(username=matching_username))  
@@ -51,24 +51,26 @@ class Yuanfenjigsaw:
     
     def get_match_result(self):
         if self.data_unavailable() :
-            return {'status_code':jiglobal.DATA_UNAVAILABLE}
+            return {'status_code':cache.get('DATA_UNAVAILABLE')}
         if self.achieve_game_times() :
-            return {'status_code':jiglobal.GAME_TIMES_REACH_THE_LIMIT,'count':0}
+            return {'status_code':cache.get('GAME_TIMES_REACH_THE_LIMIT'),'count':0}
         matching_user = self.get_matching_user()
         if matching_user != None :
 #             return {'status_code':jiglobal.MATCH_SUCCESS,'username':matching_user.user.username,'count':jiglobal.USER_GAME_COUNT.get(self.current_username)}
-            return {'status_code':jiglobal.MATCH_SUCCESS,'username':matching_user.user.username,'city':matching_user.city,'age':matching_user.age,'avatar_name':matching_user.avatar_name,'count':jiglobal.USER_GAME_COUNT.get(self.current_username)}
+            return {'status_code':cache.get('MATCH_SUCCESS'),'username':matching_user.user.username,'city':matching_user.city,'age':matching_user.age,'avatar_name':matching_user.avatar_name,'count':cache.get('USER_GAME_COUNT').get(self.current_username)}
         else :  
-            return {'status_code':jiglobal.NO_MATCHING_USER,'count':jiglobal.USER_GAME_COUNT.get(self.current_username)}
+            return {'status_code':cache.get('NO_MATCHING_USER'),'count':cache.get('USER_GAME_COUNT').get(self.current_username)}
         
 def get_count(request):
     current_username = request.user.username
-    if jiglobal.USER_GAME_COUNT.get(current_username) == None :
-        jiglobal.USER_GAME_COUNT[current_username] = jiglobal.GAME_TIMES
-    return jiglobal.USER_GAME_COUNT.get(current_username)
+    user_game_count = cache.get('USER_GAME_COUNT')
+    if user_game_count.get(current_username) == None :
+        user_game_count[current_username] = cache.get('GAME_TIMES')
+        cache.set('USER_GAME_COUNT',user_game_count)
+    return cache.get('USER_GAME_COUNT').get(current_username)
 
 def reset_game():
-    jiglobal.GIRLS = {}
-    jiglobal.BOYS = {} 
-    jiglobal.USER_GAME_COUNT = {} 
-    jiglobal.TODAY = datetime.date.today()
+    cache.set('TODAY',datetime.date.today())
+    cache.set('GIRLS',{})
+    cache.set('BOYS',{})
+    cache.set('USER_GAME_COUNT',{})
