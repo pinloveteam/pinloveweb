9282be3b7a124b2bca5d3727862fa938add4cb88
SnailJin <jin521436@163.com>
2014-01-19 23:40:57 +0800

    update-project

--- apps/common_app/models.py
+++ apps/common_app/models.py
@@ -14,3 +14,7 @@ class School(models.Model):
     SCHOOL_TYPE_CHOICE=(('1',u'国内985，美国前50'),('2',u'国内重点，美国前 200'),('3',u'国内本科，美国前500'),
                  ('4',u'国内民办本科，美国前1000'),)
     type=models.CharField(verbose_name=r'大学类型',max_length=2,choices=SCHOOL_TYPE_CHOICE,null=True)
+    class Meta:
+        verbose_name = u'学校' 
+        verbose_name_plural = u'关注表'
+        db_table = "school" 
