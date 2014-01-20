f35c9582a9fd60243a7765f2322300acebd1a643
SnailJin <jin521436@163.com>
2014-01-20 16:08:42 +0800

    facebook_app

--- apps/game_app/urls.py
+++ apps/game_app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 urlpatterns=patterns('apps.game_app.views',
     url(r'^game_pintu/$','pintu'),
     url(r'^jigsaw/$','jigsaw',name="jigsaw"),
+    url(r'^pintu_for_facebook/$','pintu_for_facebook'),
+    url(r'^pintu_for_facebook_url/$','pintu_for_facebook_url')
     
 )
--- apps/game_app/views.py
+++ apps/game_app/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import simplejson
 from apps.game_app.models import Yuanfenjigsaw, get_count
+from apps.the_people_nearby.tt import request
 
 
 
@@ -16,3 +17,18 @@ def jigsaw(request):
 def pintu(request):
     count = get_count(request)
     return render(request, 'pintu.html',{'count':count})
+
+
+'''
+获取facebook授权登录地址,跳转到回调地址（redirect_uri）
+'''
+@csrf_exempt
+def facebook_login_url(request):
+    from apps.third_party_login_app.facebook import auth_url
+    from apps.third_party_login_app.setting import FaceBookAppID
+    url=auth_url(FaceBookAppID,'http://pinpinlove.com/game/pintu_for_facebook/')
+    from django.http.response import HttpResponseRedirect
+    return HttpResponseRedirect(url)
+@csrf_exempt
+def pintu_for_facebook(request):
+    return render(request, 'pintu_for_facebook.html',)
