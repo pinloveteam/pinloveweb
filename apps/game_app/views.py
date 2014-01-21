2c4d6f615d062e898ea6da9150997576ee9eaa81
SnailJin <jin521436@163.com>
2014-01-21 13:29:51 +0800

    pintu_for_facebook

--- apps/game_app/views.py
+++ apps/game_app/views.py
@@ -31,4 +31,5 @@ def pintu_for_facebook_url(request):
     return HttpResponseRedirect(url)
 @csrf_exempt
 def pintu_for_facebook(request):
-    return render(request, 'pintu_for_facebook.html',)
+    me = request.facebook.graph.get_object('me')
+    return render(request, 'pintu_for_facebook.html',{'me':me})
