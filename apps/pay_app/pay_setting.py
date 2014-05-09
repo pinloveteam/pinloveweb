2bc41575ef433fbcf11eb81b3fe4c97aca17ddbc
SnailJin <jin521436@163.com>
2014-05-09 13:56:55 +0800

    paypal_deubg

--- apps/pay_app/pay_setting.py
+++ apps/pay_app/pay_setting.py
@@ -6,6 +6,6 @@ Created on 2014年5月8日
 '''
 ##test account:pinloveteam-facilitator@gmail.com
 PAYPAL={
-        'PAYPAL_RECEIVER_EMAIL':'pinloveteam-facilitator@gmail.com',
+        'PAYPAL_RECEIVER_EMAIL':'pinloveteam@gmail.com',
         'PAYPAL_URL':'https://pinlove.com'
         }
