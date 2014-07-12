06efc73585b01a53bed4a911728d1d031e4d863b
SnailJin <jin521436@163.com>
2014-07-12 13:42:13 +0800

    message

--- util/connection_db.py
+++ util/connection_db.py
@@ -4,14 +4,19 @@ Created on Aug 22, 2013
 
 @author: jin
 '''
-from django.db import connection
-from apps.user_app.models import UserProfile
-def connection_to_db(sql,param=None):
+from django.db import connection, transaction
+def connection_to_db(sql,param=None,type=False):
     cursor=connection.cursor();
     if param==None:
         cursor.execute(sql)
     else:
         cursor.execute(sql,param)
+    if type :
+        desc = cursor.description 
+        result=[dict(zip([col[0] for col in desc], row)) for row in cursor.fetchall()]
+        return result
+    else:
+        return cursor.fetchall()
 #     desc=cursor.description
 #     fields = [dict(zip([col[0] for col in desc], row))
 #     for row in cursor.fetchall()]
@@ -19,6 +24,13 @@ def connection_to_db(sql,param=None):
 #        for i in field_mapping:
 #             field[i[1]]=dict(i[0])[field[i[1]]]
    
-    return  cursor.fetchall()
+def connection_to_db_commit(sql,param=None):
+    cursor=connection.cursor();
+    if param==None:
+        cursor.execute(sql)
+    else:
+        cursor.execute(sql,param)
+    transaction.commit_unless_managed()
+  
     
 
