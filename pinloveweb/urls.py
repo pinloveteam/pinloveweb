b7c1bcee6dae5a31fae7c0c8d4e16e2be2778cb8
SnailJin <jin521436@163.com>
2014-07-19 13:30:06 +0800

    message_debug

--- pinloveweb/urls.py
+++ pinloveweb/urls.py
@@ -40,8 +40,7 @@ urlpatterns = patterns('',
     url(r'^account/verification/$', 'pinloveweb.views.register_verify'), 
     url(r'^account/check_register/$', 'pinloveweb.views.check_register'),
    url(r'^newCount/$', 'pinloveweb.views.newcount'),
-   #印证通道
-   url(r'^/pusher/auth$', 'apps.pusher_app.views.pusher_authentication'),
+
    
     
     # User Profile 
