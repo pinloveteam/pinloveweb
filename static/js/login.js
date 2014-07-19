26838be446de28910971362c923712b5161d701b
SnailJin <jin521436@163.com>
2014-07-19 15:02:48 +0800

    login debug

--- static/js/login.js
+++ static/js/login.js
@@ -3,6 +3,8 @@ function submit(){
 }
 
 $(document).ready(function() {
+     var height=document.body.clientHeight;
+	 $('.stage').css('height',height-50);
 	$.scrollUp({
 		scrollName : 'scrollUp', // Element ID
 		topDistance : '300', // Distance from top before showing element (px)
