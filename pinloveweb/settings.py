aa44078047d01f7bb0943ae22c7cea8c6e38dc0b
SnailJin <jin521436@163.com>
2014-08-05 15:20:42 +0800

    传头

--- pinloveweb/settings.py
+++ pinloveweb/settings.py
@@ -350,33 +350,33 @@ CACHES = {
     },
 }
 # 本地环境
-# DATABASES = {
-#                              
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'django',                      # Or path to database file if using sqlite3.
-#                                                 # The following settings are not used with sqlite3:
-#         'USER': 'root',                         #pinloveteam
-#         'PASSWORD': 'jin521436',                       #redyellowblue123#
-#         'HOST': '',                             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-#         'PORT': '',      
-#                                                 # Set to empty string for default.
-#     }
-# }
-# MEDIA_URL = '/media/'
-# STATIC_URL = '/static/'
-# MEDIA_ROOT = os.path.join(os.path.dirname(PATH),'update').replace('\\','/')
-# STATIC_ROOT = os.path.join(PATH,'static').replace('\\','/')
-# UPLOAD_AVATAR_UPLOAD_ROOT= os.path.join(os.path.dirname(PATH),'update/user_img').replace('\\','/')
-# UPLOAD_AVATAR_AVATAR_ROOT= os.path.join(os.path.dirname(PATH),'update/user_img').replace('\\','/')
-# UPLOAD_AVATAR_URL_PREFIX_ORIGINAL='/media/user_img/'
-# UPLOAD_AVATAR_URL_PREFIX_CROPPED='/avatar/'
-# DOMAIN='pinlove.xicp.net'
+DATABASES = {
+                              
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'django',                      # Or path to database file if using sqlite3.
+                                                # The following settings are not used with sqlite3:
+        'USER': 'root',                         #pinloveteam
+        'PASSWORD': 'jin521436',                       #redyellowblue123#
+        'HOST': '',                             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',      
+                                                # Set to empty string for default.
+    }
+}
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(os.path.dirname(PATH),'update').replace('\\','/')
+STATIC_ROOT = os.path.join(PATH,'static').replace('\\','/')
+UPLOAD_AVATAR_UPLOAD_ROOT= os.path.join(os.path.dirname(PATH),'update/user_img').replace('\\','/')
+UPLOAD_AVATAR_AVATAR_ROOT= os.path.join(os.path.dirname(PATH),'update/user_img').replace('\\','/')
+UPLOAD_AVATAR_URL_PREFIX_ORIGINAL='/media/user_img/'
+UPLOAD_AVATAR_URL_PREFIX_CROPPED='/avatar/'
+DOMAIN='pinlove.xicp.net'
 
 
 #---服务器环境-----
 DATABASES = {
-                                
+                                 
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pinlove_db_1',                      # Or path to database file if using sqlite3.
@@ -390,13 +390,13 @@ DATABASES = {
 }
 #upload 上传地址
 MEDIA_URL = '/update/'
-MEDIA_ROOT ='/home/pinloveteam/webapps/pinlove/pinloveweb/update'
+MEDIA_ROOT ='/home/pinloveteam/webapps/pinlove/update'
 #静态文件地址
 STATIC_ROOT = '/home/pinloveteam/webapps/pinlove_static'
 STATIC_URL = '/static/'
 #头像上传地址
-UPLOAD_AVATAR_UPLOAD_ROOT='/home/pinloveteam/webapps/pinlove/pinloveweb/update/user_img'
-UPLOAD_AVATAR_AVATAR_ROOT='/home/pinloveteam/webapps/pinlove/pinloveweb/update/user_img'
+UPLOAD_AVATAR_UPLOAD_ROOT='/home/pinloveteam/webapps/pinlove/update/user_img'
+UPLOAD_AVATAR_AVATAR_ROOT='/home/pinloveteam/webapps/pinlove/update/user_img'
 UPLOAD_AVATAR_URL_PREFIX_ORIGINAL='/media/user_img/'
 #grappelli
 UPLOAD_AVATAR_URL_PREFIX_CROPPED='/avatar/'
