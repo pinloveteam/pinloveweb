9172f50f3574bf3d6712451c63809767132e5062
SnailJin <jin521436@163.com>
2014-09-18 23:25:02 +0800

    task

--- apps/task_app/tasks.py
+++ apps/task_app/tasks.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from apps.user_app.models import UserProfile
 
 '''
 计算收入
 '''
+from apps.user_app.models import UserProfile
+from apps.recommend_app.models import Grade
 def cal_income_task():
     userProfileList=UserProfile.objects.exclude(income=-1)
     for userProfile in userProfileList:
         from apps.recommend_app.recommend_util import cal_income
-        cal_income(userProfile.income,userProfile.gender)
+        incomescore=cal_income(userProfile.income,userProfile.gender)
+        Grade.objects.filter(user_id=userProfile.user_id).update(incomescore=incomescore)
