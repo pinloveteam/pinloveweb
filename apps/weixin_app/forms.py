2e938dc0fe9b3fd8eec683f300ecf26aebe53f9d
SnailJin <jin521436@163.com>
2015-04-14 14:47:30 +0800

    weixin

--- apps/weixin_app/forms.py
+++ apps/weixin_app/forms.py
@@ -14,7 +14,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 EDUCATION_CHIOCES=((0,u'莫以读书论英雄（本科以下）'),(1,u'学历有限（大专）'),(2,u'十年寒窗（本科）'),(3,u'学有专攻（硕士)'),(4,u'才高八斗（博士'))
 HEIGHT_CHIOCES=((185,u'鹤立鸡群（男大于1.85， 女大于1.75）'),(175,u' 玉树凌风（男大于1.75，女大于1.65）'),
                 (170,u'俊秀挺拔（男大于1.7，女大于1.6）'),(160,u'及格线上（男大于1.6， 女1大于1.5）'),(155,u'短小精悍（男小于1.6， 女1小于1.5）'))
-SCHOOL_CHIOCES=((4,u'如雷贯耳(985学校)'),(3,u'百年学府(重点学校)'),(2,u'中流砥柱(本科学校)'),(1,u'马马虎虎(专科学校)'),(0,u'布鲁弗莱（专科一下）'))
+SCHOOL_CHIOCES=((4,u'如雷贯耳(985学校)'),(3,u'百年学府(重点学校)'),(2,u'中流砥柱(本科学校)'),(1,u'马马虎虎(专科学校)'),(0,u'布鲁弗莱（专科以下）'))
 
 INCOME_CHIOCES=((99,u'钻石王老五(年薪>100w)'),(50,u'金领(年薪>50w)'),(30,u'蓝领(年薪>30w)'),(10,u'白领(年薪>10w)'),(5,u'无领白领(年薪>5w)'))
 class InfoForm (ModelForm) : 
@@ -27,7 +27,7 @@ class InfoForm (ModelForm) :
             HEIGHT_CHIOCES_LIST=[ (height[0]-5,height[1]) for height in HEIGHT_CHIOCES]
         self.fields['height'].choices=HEIGHT_CHIOCES_LIST
         self.fields['income'].choices=INCOME_CHIOCES
-    schoolType=forms.ChoiceField(choices=SCHOOL_CHIOCES)   
+    schoolType=forms.TypedChoiceField(label=u"学校类型",choices=SCHOOL_CHIOCES)   
     class Meta : 
         model = UserProfile  
         fields = ( 'height',  'income','education',)
