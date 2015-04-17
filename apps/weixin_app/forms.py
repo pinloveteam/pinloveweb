32634197dfc91e76d889da0c10ce666ce697afb7
SnailJin <jin521436@163.com>
2015-04-17 14:39:15 +0800

    微信游戏

--- apps/weixin_app/forms.py
+++ apps/weixin_app/forms.py
@@ -11,9 +11,11 @@ from django import forms
 from django.forms.forms import Form
 from apps.recommend_app.models import Grade
 from django.core.validators import MaxValueValidator, MinValueValidator
-EDUCATION_CHIOCES=((0,u'莫以读书论英雄（本科以下）'),(1,u'学历有限（大专）'),(2,u'十年寒窗（本科）'),(3,u'学有专攻（硕士)'),(4,u'才高八斗（博士'))
-HEIGHT_CHIOCES=((185,u'鹤立鸡群（男大于1.85， 女大于1.75）'),(175,u' 玉树凌风（男大于1.75，女大于1.65）'),
-                (170,u'俊秀挺拔（男大于1.7，女大于1.6）'),(160,u'及格线上（男大于1.6， 女1大于1.5）'),(155,u'短小精悍（男小于1.6， 女1小于1.5）'))
+EDUCATION_CHIOCES=((0,u'莫以读书论英雄本科以下）'),(1,u'学历有限（大专）'),(2,u'十年寒窗（本科）'),(3,u'学有专攻（硕士)'),(4,u'才高八斗（博士'))
+MAN_HEIGHT_CHIOCES=((185,u'鹤立鸡群（男大于1.85）'),(175,u' 玉树凌风（男大于1.75)'),
+                (170,u'俊秀挺拔(男大于1.7)'),(160,u'及格线上(男大于1.6)'),(155,u'短小精悍(男小于1.6)'))
+FEMAN_HEIGHT_CHIOCES=((175,u'鹤立鸡群（女大于1.75)'),(165,u' 玉树凌风（女大于1.65)'),
+                (160,u'俊秀挺拔（女大于1.6)'),(150,u'及格线上（女大于1.55)'),(145,u'短小精悍（女小于1.5)'))
 SCHOOL_CHIOCES=((4,u'如雷贯耳(985学校)'),(3,u'百年学府(重点学校)'),(2,u'中流砥柱(本科学校)'),(1,u'马马虎虎(专科学校)'),(0,u'布鲁弗莱（专科以下）'))
 
 INCOME_CHIOCES=((99,u'钻石王老五(年薪>100w)'),(50,u'金领(年薪>50w)'),(30,u'蓝领(年薪>30w)'),(10,u'白领(年薪>10w)'),(5,u'无领白领(年薪>5w)'))
@@ -21,11 +23,10 @@ class InfoForm (ModelForm) :
     def __init__(self, *args, **kwargs):
         super(InfoForm, self).__init__(*args, **kwargs)
         self.fields['education'].choices=EDUCATION_CHIOCES
-        global HEIGHT_CHIOCES
-        HEIGHT_CHIOCES_LIST=HEIGHT_CHIOCES
         if self.instance.gender=='F':
-            HEIGHT_CHIOCES_LIST=[ (height[0]-5,height[1]) for height in HEIGHT_CHIOCES]
-        self.fields['height'].choices=HEIGHT_CHIOCES_LIST
+            self.fields['height'].choices=FEMAN_HEIGHT_CHIOCES
+        else:
+            self.fields['height'].choices=MAN_HEIGHT_CHIOCES
         self.fields['income'].choices=INCOME_CHIOCES
     schoolType=forms.TypedChoiceField(label=u"学校类型",choices=SCHOOL_CHIOCES)   
     class Meta : 
@@ -45,14 +46,11 @@ class InfoForm (ModelForm) :
 class TaInfoForm(ModelForm) :
     def __init__(self, *args, **kwargs):
         super(TaInfoForm, self).__init__(*args, **kwargs)
-        global HEIGHT_CHIOCES
-        HEIGHT_CHIOCES_LIST=HEIGHT_CHIOCES
         if self.initial.get('gender')=='M':
-            HEIGHT_CHIOCES_LIST=[ (height[0]-5,height[1]) for height in HEIGHT_CHIOCES]
-            self.fields['expectHeight'].choices=HEIGHT_CHIOCES_LIST
+            self.fields['expectHeight'].choices=FEMAN_HEIGHT_CHIOCES
         for field in ['heightweight','incomeweight','educationweight','characterweight']:
             self.fields[field].validators=[MinValueValidator(0), MaxValueValidator(100)]
-    expectHeight=forms.ChoiceField(choices=HEIGHT_CHIOCES) 
+    expectHeight=forms.ChoiceField(choices=MAN_HEIGHT_CHIOCES) 
         
     def cal_weight(self,userId):
         '''
